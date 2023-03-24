db90118c55f3a9c1257db391d7d37244ca33c164
AbdulNasirQader <nasakanasir1@gmail.com>
2023-03-24 19:59:43 -0400

    Java Review

---
+++ src/ReviewClass12/LowBalanceException.java
@@ -0,0 +1,9 @@
+package ReviewClass12;
+
+public class LowBalanceException extends RuntimeException{
+
+        LowBalanceException(String msg){
+            super(msg);
+        }
+
+}
---
+++ src/ReviewClass12/ThrowKeyword.java
@@ -0,0 +1,12 @@
+package ReviewClass12;
+
+public class ThrowKeyword {
+
+    public static void main(String[] args) {
+
+        throwDemo();
+    }
+    static void throwDemo(){
+        throw new NullPointerException();
+    }
+}
